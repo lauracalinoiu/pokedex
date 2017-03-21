400e5cf75a7e870708dfbdec273520fae5dd816d
Laura Calinoiu <lauramocanu16@gmail.com>
2017-03-21 10:18:26 +0200

    Set layout

---
+++ PokeDex/PokeCell.swift
@@ -0,0 +1,16 @@
+//
+//  PokeCell.swift
+//  PokeDex
+//
+//  Created by Laura Calinoiu on 21/03/2017.
+//  Copyright © 2017 Laura Calinoiu. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
